f7c9d57852e74ec0812a2f471f59d23aaf40c270
elimerrell <elijah.merrell@gmail.com>
2019-05-06 11:42:11 -0700

    updating controllers

--- app/serializers/sighting_serializer.rb
+++ app/serializers/sighting_serializer.rb
@@ -1,4 +1,4 @@
 class SightingSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :whale_sighting_id, :species, :quantity, :orca_type, :description, :latitude, :longitude, :location, :photo
+  attributes :id, :user_id, :whale_sighting_id, :species, :quantity, :orca_type, :description, :latitude, :longitude, :location
 end
 
